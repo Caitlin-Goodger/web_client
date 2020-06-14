151c90d85280668d6d18f2243b977e8dbebe12cc
goodgecait <goodgecait@ecs.vuw.ac.nz>
2020-06-14 14:05:06 +1200

    Added method to return a random Level

--- src/main/java/nz/ac/vuw/swen301/a2/client/CreateRandomLogs.java
+++ src/main/java/nz/ac/vuw/swen301/a2/client/CreateRandomLogs.java
@@ -1,4 +1,34 @@
 package nz.ac.vuw.swen301.a2.client;
 
+import org.apache.log4j.*;
+import java.lang.Math.*;
+
 public class CreateRandomLogs {
+
+
+    private Level getRandomLevel() {
+        int randomNumber = (int) (Math.random()*8);
+
+        if(randomNumber == 1) {
+            return Level.ALL;
+
+        } else if(randomNumber == 2) {
+            return Level.TRACE;
+        } else if(randomNumber == 3) {
+            return Level.DEBUG;
+        } else if(randomNumber == 4) {
+            return Level.INFO;
+        } else if(randomNumber == 5) {
+            return Level.WARN;
+        } else if(randomNumber == 6) {
+            return Level.ERROR;
+        } else if(randomNumber == 7) {
+            return Level.FATAL;
+        } else {
+            return Level.OFF;
+        }
+    }
+
 }
+
+
