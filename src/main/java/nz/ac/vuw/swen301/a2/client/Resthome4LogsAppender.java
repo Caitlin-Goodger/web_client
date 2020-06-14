f0a804520120419bc17cb0e2aa49a6cd8e70442a
goodgecait <goodgecait@ecs.vuw.ac.nz>
2020-06-14 13:36:58 +1200

    Made Resthome4LogsAppender extend AppenderSkelton and created the required method stubs and constructor to implement

--- src/main/java/nz/ac/vuw/swen301/a2/client/Resthome4LogsAppender.java
+++ src/main/java/nz/ac/vuw/swen301/a2/client/Resthome4LogsAppender.java
@@ -1,4 +1,27 @@
 package nz.ac.vuw.swen301.a2.client;
 
-public class Resthome4LogsAppender {
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.spi.LoggingEvent;
+
+public class Resthome4LogsAppender extends AppenderSkeleton {
+
+
+    public Resthome4LogsAppender() {
+
+    }
+
+    @Override
+    protected void append(LoggingEvent event) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public boolean requiresLayout() {
+        return false;
+    }
 }
